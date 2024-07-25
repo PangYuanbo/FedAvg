195f1be48bd8077531cafd0409036f56375ebbdd
PangYuanbo <yuanbopang@outlook.com>
2024-07-24 23:21:39 -0700

    Multiprocess——CPU

--- train_test.py
+++ train_test.py
@@ -2,6 +2,21 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from models import DoubleNN,CNN
+
+def train_process(number,clients_process,models,data,B,E,l,global_model,queue):
+    for client_idx, client_model in enumerate(clients_process):
+        for param, center_param in zip(models[client_model].parameters(), global_model.parameters()):
+            param.data = center_param.data.clone()
+        dataloader = DataLoader(data[number+client_idx],batch_size=B, shuffle=True)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(models[client_model].parameters(), lr=l)
+        train(models[client_model], dataloader, criterion, optimizer, E)
+        # print(f"Client {client_model} trained")
+    # print ("clients_process",len(clients_process))
+    # print("models",len(models))
+    trained_params = {client_model: models[client_model].state_dict() for  client_model in clients_process}
+    queue.put(trained_params)
 
 def train(model, trainloader, criterion, optimizer, epochs=10):
     model.train()
