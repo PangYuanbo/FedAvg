0ec5d748dff2c03d1b53a6a711ddc3b2062f7326
PangYuanbo <yuanbopang@outlook.com>
2024-08-18 00:40:26 -0700

    update

--- Backdoor/attack_train.py
+++ Backdoor/attack_train.py
@@ -3,23 +3,21 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-'''
-    attack_method:
-    1. Pixel-backdoors
-    2. Semantic-backdoors
-    3. LF-backdoors
-'''
 
-
-def attack_process(number, id, clients_process, models, data, B, E, l, global_model, queue, attack_method,device):
-
+def attack_process(number, id, clients_process, models, data, B, E, l, global_model, queue, attack_method, device):
     for client_idx, client_model in enumerate(clients_process):
+        # 同步模型参数
         for param, center_param in zip(models[client_model].parameters(), global_model.parameters()):
             param.data = center_param.data.clone()
+
         dataloader = DataLoader(data[number + client_idx], batch_size=B, shuffle=True)
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(models[client_model].parameters(), lr=l)
-        train(models[client_model], dataloader, criterion, optimizer,device, E)
+
+        # 模型训练
+        train(models[client_model], dataloader, criterion, optimizer, device, E)
+
+        # 执行攻击方法
         if attack_method == "Pixel-backdoors":
             pass
         elif attack_method == "Semantic-backdoors":
@@ -31,31 +29,36 @@ def attack_process(number, id, clients_process, models, data, B, E, l, global_mo
                                                        client_model].fc1.weight - global_model.fc1.weight) * 20 + global_model.fc1.weight
                 models[client_model].fc1.bias = (models[
                                                      client_model].fc1.bias - global_model.fc1.bias) * 20 + global_model.fc1.bias
+
+    # 将训练好的参数传回主进程
     trained_params = {client_model: models[client_model].state_dict() for client_model in clients_process}
     queue.put(trained_params)
-    print("number", id)
+    print("Completed attack process for:", id)
     return
 
 
-def train_process(number, id, clients_process, models, data, B, E, l, global_model, queue,device):
+def train_process(number, id, clients_process, models, data, B, E, l, global_model, queue, device):
     for client_idx, client_model in enumerate(clients_process):
+        # 同步模型参数
         for param, center_param in zip(models[client_model].parameters(), global_model.parameters()):
             param.data = center_param.data.clone()
+
         dataloader = DataLoader(data[number + client_idx], batch_size=B, shuffle=True)
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(models[client_model].parameters(), lr=l,momentum=0.9, weight_decay=5e-4)
-        train(models[client_model], dataloader, criterion, optimizer, device,epochs=E)
-        # print(f"Client {client_model} trained")
-    # print ("clients_process",len(clients_process))
+        optimizer = optim.SGD(models[client_model].parameters(), lr=l, momentum=0.9, weight_decay=5e-4)
+
+        # 模型训练
+        train(models[client_model], dataloader, criterion, optimizer, device, epochs=E)
+
+    # 将训练好的参数传回主进程
     trained_params = {client_model: models[client_model].state_dict() for client_model in clients_process}
     queue.put(trained_params)
-    print("number", id)
+    print("Completed training process for:", id)
     return
 
 
-def train(model, trainloader, criterion, optimizer,device, epochs=10):
-    print("device",device)
-    global running_loss
+def train(model, trainloader, criterion, optimizer, device, epochs=10):
+    print("Training on device:", device)
     model.to(device)  # 将模型移动到设备上
     model.train()  # 设置模型为训练模式
 
@@ -63,7 +66,6 @@ def train(model, trainloader, criterion, optimizer,device, epochs=10):
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             inputs, labels = data[0].to(device), data[1].to(device)  # 将数据移动到设备上
-            # inputs, labels = data[0], data[1]  # 将数据移动到设备上
             optimizer.zero_grad()  # 清除优化器的梯度
             outputs = model(inputs)  # 前向传播
             loss = criterion(outputs, labels)  # 计算损失
@@ -72,17 +74,11 @@ def train(model, trainloader, criterion, optimizer,device, epochs=10):
 
             running_loss += loss.item()
 
-    print("total_loss",running_loss/len(trainloader))
+    print("Total loss:", running_loss / len(trainloader))
     model.to("cpu")  # 将模型移动回CPU
 
 
-def test(model, testloader,device):
-    # 检查MPS设备是否可用
-    # if torch.backends.mps.is_available():
-    #     device = torch.device("mps")
-    # else:
-    #     device = torch.device("cpu")
-
+def test(model, testloader, device):
     model.to(device)  # 将模型移动到设备上
     model.eval()  # 设置模型为评估模式
 
@@ -95,6 +91,7 @@ def test(model, testloader,device):
             _, predicted = torch.max(outputs.data, 1)  # 获取预测结果
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+
     model.to("cpu")  # 将模型移动回CPU
     accuracy = 100 * correct / total
     print(f'Accuracy of the network on the test images: {accuracy}%')
