6057431d74bd9d11e4f0c11497f9c9068603c157
PangYuanbo <yuanbopang@outlook.com>
2024-08-19 23:48:16 -0700

    merge

--- Backdoor/attack_train.py
+++ Backdoor/attack_train.py
@@ -55,6 +55,7 @@ def train_process(number, id,event, clients_process, models, data, B, E, l, glob
             if not isinstance(trained_model, torch.nn.Module):
                 raise TypeError(
                     f"Expected trained_model to be a torch.nn.Module, but got {type(trained_model)} instead.")
+            print(type(trained_model))
             # print("Trained models:", id(trained_models[client_model]))
 
          # 将训练好的参数转移到CPU后再传递
